33d8a5c72f469295fa48d87900d091239db50349
Kse <kh@gmail.com>
2022-01-07 23:23:39 -0500

    Ver 0.2

--- lib/components/screen/home/components/body.dart
+++ lib/components/screen/home/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -39,7 +40,145 @@ class Body extends StatelessWidget {
               ),
             ],
           ),
-        )
+        ),
+
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        height: 0.05.sh,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Expanded(
+            child: Row(
+              children: [
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        ),
+                        
+                      ),
+                      Center(child: Text('Nike', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.grey[200],
+                        //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        // ),
+                        
+                      ),
+                      Center(child: Text('Adidas', style: TextStyle(color:Colors.grey[400],fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.grey[200],
+                        //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        // ),
+                        
+                      ),
+                      Center(child: Text('Jordan', style: TextStyle(color:Colors.grey[400],fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.grey[200],
+                        //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        // ),
+                        
+                      ),
+                      Center(child: Text('Puma', style: TextStyle(color:Colors.grey[400] ,fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.grey[200],
+                        //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        // ),
+                        
+                      ),
+                      Center(child: Text('Reebooee', style: TextStyle(color:Colors.grey[400] ,fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 0.1.sh,
+                  width: 0.2.sw,
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 0.02.sh),
+                        alignment: Alignment.bottomCenter,
+                        height: 0.025.sh,
+                        // decoration: BoxDecoration(
+                        //   color: Colors.grey[200],
+                        //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+                        // ),
+                        
+                      ),
+                      Center(child: Text('Reebooee', style: TextStyle(color:Colors.grey[400] ,fontSize: 22, fontWeight: FontWeight.w600),)),
+                      
+                    ],
+                  ),
+                )
+
+                
+              ],
+            ),
+          ),
+        ),
+      )
+
+
       ],
     ));
   }
